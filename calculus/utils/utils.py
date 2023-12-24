d9ee1413306254a0d9460ac1217b5d741b438be5
Xiaoxin Yin <xiaoxin@gmail.com>
2023-12-23 22:11:32 -0800

    Evaluate results of symbolic regression on integrals

--- calculus/utils/utils.py
+++ calculus/utils/utils.py
@@ -6,6 +6,70 @@ import functools
 import random
 from sympy import sympify
 from sympy.functions import Abs
+from sympy.core.rules import Transform
+
+class TimeoutError(Exception):
+    pass
+
+def timeout(seconds=15, error_message=os.strerror(errno.ETIME)):
+    def decorator(func):
+        def _handle_timeout(signum, frame):
+            raise TimeoutError(error_message)
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            signal.signal(signal.SIGALRM, _handle_timeout)
+            signal.alarm(seconds)
+            try:
+                result = func(*args, **kwargs)
+            finally:
+                signal.alarm(0)
+            return result
+        return wrapper
+    return decorator
+
+def round_all_floats(f, num_digits=2):
+    return f.xreplace(Transform(lambda x: x.round(num_digits), lambda x: isinstance(x, sp.Float)))
+
+def get_all_constants(expr):
+    return [t for t in expr.atoms() if t not in expr.free_symbols]
+
+def get_coefficients_and_exponents(f):
+    variables = list(f.free_symbols)
+    assert len(variables)<=1, "Expression having multiple variable " + str(f)
+    if len(variables) == 0:
+        return list()
+    t = variables[0]
+    return [[float(x) for x in term.as_coeff_exponent(t)] for term in f.as_ordered_terms()]
+
+# Return coeffs of a polynomial from lowest power to highest
+def get_polynomial_coeffs(f, max_power=6):
+    result = [0.0]*(max_power+1)
+    coeff_pairs = get_coefficients_and_exponents(f)
+    for p in coeff_pairs:
+        coeff, exp = float(p[0]), float(p[1])
+        idx = int(round(exp))
+        if idx < 0 or idx > max_power:
+            continue
+        result[idx] = coeff
+    return result
+
+# Only keep the polynomial part
+@timeout(5)
+def filter_non_polynomial(f):
+    variables = list(f.free_symbols)
+    assert len(variables)<=1, "Expression having multiple variable " + str(f)
+    if len(variables) == 0:
+        return sympify(0)
+    t = variables[0]
+    terms_kept = []
+    terms = f.as_ordered_terms()
+    for term in terms:
+        coeff, exp = term.as_coeff_exponent(t)
+        if coeff.is_constant():
+            terms_kept.append(term)
+    if len(terms_kept) == 0:
+        return sympify(0)
+    return sum(terms_kept)
 
 def shrink_a_number(n):
     if n.is_Integer and Abs(n) > 20:
@@ -63,21 +127,3 @@ def randomize(expr, prob_change=0.5, shrink=False, remove_items=False):
     else:
         return expr_subed
 
-class TimeoutError(Exception):
-    pass
-
-def timeout(seconds=15, error_message=os.strerror(errno.ETIME)):
-    def decorator(func):
-        def _handle_timeout(signum, frame):
-            raise TimeoutError(error_message)
-        @functools.wraps(func)
-        def wrapper(*args, **kwargs):
-            signal.signal(signal.SIGALRM, _handle_timeout)
-            signal.alarm(seconds)
-            try:
-                result = func(*args, **kwargs)
-            finally:
-                signal.alarm(0)
-            return result
-        return wrapper
-    return decorator
